b0bf2cd0b82a943c439f672c185cd20c40dbe80a
jp <jpc.araujo@hotmail.com>
2019-02-24 00:54:30 -0300

    Configuração do Vuex para gerenciamento do estado da aplicação, refatoração do componente Stock para receber o array gerenciado pelo Vuex

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -9,15 +9,9 @@
 
     export default {
         components:{ Stock },
-        data(){
-            return{
-                stocks: [
-                    { id: 1 , name: 'CD Projekt Red', price: 120},
-                    { id: 2 , name: 'Insomniac Games', price: 150},
-                    { id: 3 , name: 'Bandai', price: 200},
-                    { id: 4 , name: 'Santa Monica', price: 220},
-                    { id: 5 , name: 'Nintendo', price: 215},
-                ]
+        computed:{
+            stocks(){
+                return this.$store.getters.stocks
             }
         }
     }
---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1 , name: 'CD Projekt Red', price: 120},
+    { id: 2 , name: 'Insomniac Games', price: 150},
+    { id: 3 , name: 'Bandai', price: 200},
+    { id: 4 , name: 'Santa Monica', price: 220},
+    { id: 5 , name: 'Nintendo', price: 215},
+]
