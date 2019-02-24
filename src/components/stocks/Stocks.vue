431310d481024428d1e5b0b81eacbb8b8767dbf5
jp <jpc.araujo@hotmail.com>
2019-02-24 00:08:30 -0300

    Componentes Stocks e Stock, carregando lista de Stock em Stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,25 @@
 <template>
-    
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id"/>
+    </v-layout>
 </template>
 
 <script>
+    import Stock from "./Stock";
+
     export default {
-        name: "Stocks"
+        components:{ Stock },
+        data(){
+            return{
+                stocks: [
+                    { id: 1 , name: 'CD Projekt Red', price: 120},
+                    { id: 2 , name: 'Insomniac Games', price: 150},
+                    { id: 3 , name: 'Bandai', price: 200},
+                    { id: 4 , name: 'Santa Monica Studios', price: 220},
+                    { id: 5 , name: 'Nintendo', price: 215},
+                ]
+            }
+        }
     }
 </script>
 
