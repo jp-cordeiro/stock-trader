5ffae8f6a8a56e7a3c766919d123ac9e68e0ce92
jp <jpc.araujo@hotmail.com>
2019-02-24 00:36:08 -0300

    Valores de estado interno do componente Stock, validações botão comprar

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout row wrap>
-        <Stock v-for="stock in stocks" :key="stock.id"/>
+        <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"/>
     </v-layout>
 </template>
 
@@ -15,7 +15,7 @@
                     { id: 1 , name: 'CD Projekt Red', price: 120},
                     { id: 2 , name: 'Insomniac Games', price: 150},
                     { id: 3 , name: 'Bandai', price: 200},
-                    { id: 4 , name: 'Santa Monica Studios', price: 220},
+                    { id: 4 , name: 'Santa Monica', price: 220},
                     { id: 5 , name: 'Nintendo', price: 215},
                 ]
             }
